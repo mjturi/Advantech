8c1d84e494f2487219feec5e133cfc25842c746d
mjturi <56698233+mjturi@users.noreply.github.com>
2020-11-18 10:15:55 -0800

    Update and rename pagecon.py to page_con.py

--- linkedin/page_con.py
+++ linkedin/page_con.py
@@ -1,3 +1,4 @@
+# necessary imports
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
@@ -13,7 +14,8 @@ def bot():
     driver = webdriver.Chrome(options=options, executable_path=driver_path)
     actions = ActionChains(driver)
     driver.maximize_window()
-
+    
+    # load into linkedin
     driver.get('https://www.linkedin.com/')
     time.sleep(1)
 
@@ -23,9 +25,9 @@ def bot():
 
     # enter login info
     user = driver.find_element_by_xpath('/html/body/div/main/div[2]/form/div[1]/input')
-    user.send_keys('EMAIL')
+    user.send_keys('EMAIL') # replace with user credentials
     pswd = driver.find_element_by_xpath('/html/body/div/main/div[2]/form/div[2]/input')
-    pswd.send_keys('PASSWORD')
+    pswd.send_keys('PASSWORD') # replace with user credentials
 
     # submit form, try catch because it was having issues finding the button by a single absolute path
     try:
@@ -51,35 +53,32 @@ def bot():
     time.sleep(2)
 
     main_window = driver.current_window_handle
-
+    
+    # open linkedin admin page
     driver.execute_script('''window.open("https://www.linkedin.com/mynetwork/?lipi=urn%3Ali%3Apage%3Ad_flagship3_feed%3BNGEpzm4cQvOwXtNhBJIMqA%3D%3D&licu=urn%3Ali%3Acontrol%3Ad_flagship3_feed-nav.mynetwork","_blank");''')
-
+    
+    # switch to new tab
     driver.switch_to.window(driver.window_handles[1])
 
     time.sleep(2)
-
+    
+    # block to navigate to the invite new connections to the company li page, filters to only BAH people
     driver.find_element_by_xpath('/html/body/div[8]/div[3]/div/div/div/div/div/aside/div/div/div[1]/section/div/div[1]/a/div/div[1]/li-icon').click()
     time.sleep(2)
-
     driver.find_element_by_xpath('/html/body/div[8]/div[3]/div/div/div/div/div/div/div/div/section/div/div[2]/a').click()
     time.sleep(2)
-
     driver.find_element_by_xpath('/html/body/div[8]/div[3]/div/div[1]/header/div/div/div[2]/div/div/div[2]/ul/li[2]/form/button').click()
     time.sleep(2)
-
     driver.find_element_by_xpath('/html/body/div[8]/div[3]/div/div[1]/header/div/div/div[2]/div/div/div[2]/ul/li[2]/form/div/fieldset/div/ul/li[2]/label/p/span').click()
     time.sleep(2)
-
     driver.find_element_by_xpath('/html/body/div[8]/div[3]/div/div[1]/header/div/div/div[2]/div/div/div[2]/ul/li[2]/form/div/fieldset/div/div[2]/button[2]').click()
     time.sleep(2)
 
     all_names = []
 
-    #NOT SURE WHY I CANNOT GRAB THEIR NAMES.......
-
     li_count = 1
     max_names = 96
-    while True:
+    while True: # block to invite each person and scroll through the list until the connections limit is reached
         if li_count == 9:
             print('scrolling')
             driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
@@ -90,7 +89,6 @@ def bot():
         temp = temp.text
         all_names.append(temp)
         li_count += 1
-        # driver.execute_script("window.scrollTo(0, " + str(li_count*100) + ")")
         if len(all_names) > 96:
             print('no more pages')
             driver.switch_to.window(main_window)
@@ -123,41 +121,5 @@ def bot():
                 except Exception:
                     print('no more pages')
                     driver.switch_to.window(main_window)
-                    # driver.find_element_by_xpath(
-                    #     '/html/body/div[7]/div[3]/div/div[6]/main/div[1]/div[3]/div[3]/div[1]/section/footer/button').click()
-                    # con_box = driver.find_element_by_xpath(
-                    #     '/html/body/div[4]/div/div/div[1]/div/div[2]/div/div/div[1]/div/input')
-                    # for name in all_names:
-                    #     print(name)
-                    #     con_box.send_keys(name)
-                    #     driver.find_element_by_xpath(
-                    #         '/html/body/div[4]/div/div/div[1]/div/div[2]/div/div/div[2]').click()
-                    # time.sleep(15)
-                    # driver.find_element_by_xpath('/html/body/div[4]/div/div/div[3]/div/button').click()
-
-
-    # WORKS, Just need a for loop to iterate through each and then click the next page button
-
-    # while True:
-    #     for i in range(1, 11):
-    #          #                                   '/html/body/div[8]/div[3]/div/div[2]/div/div[2]/div/div/div/div/ul/li[1]/div/div/div[2]/a/h3/span/span/span[1]'
-    #         name = driver.find_element_by_xpath('/html/body/div[8]/div[3]/div/div[2]/div/div[2]/div/div/div/div/ul/li[' + str(i) + ']/div/div/div[2]/a/h3/span/span/span[1]')
-    #         all_names.append(name.text)
-    #         print(all_names)
-    #     try:
-    #         driver.find_element_by_xpath('/html/body/div[8]/div[3]/div/div[2]/div/div[2]/div/div/div/div/div[1]/div/button[2]/span').click()
-    #     except Exception:
-    #         break
-
-    # ember1482 > span > span > span.name.actor-name
-
-    # HERE I JUST NEED TO KNOW WHAT FILTERS WE ARE LOOKING FOR FROM CONNECTIONS...
-
-
-    # data[link_counter].send_keys(Keys.CONTROL + Keys.RETURN)
-    #
-    # driver.switch_to.window(driver.window_handles[1])
-    #
-    # time.sleep(2)
 
 bot()
