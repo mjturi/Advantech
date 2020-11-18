c54f7a6ed767888c1098cf66a69f79f28dba0c71
mjturi <56698233+mjturi@users.noreply.github.com>
2020-11-18 09:31:13 -0800

    Update nation_data_merge.py

--- data_clean/nation_data_merge.py
+++ data_clean/nation_data_merge.py
@@ -1,14 +1,18 @@
+# necessary imports
 import pandas as pd
 
-df = pd.read_csv('csv')
+# read as many previously scraped csv's as needed
+df = pd.read_csv('csv') # df needs to be the original master dataframe that you are adding to
 df2 = pd.read_csv('csv')
 df3 = pd.read_csv('csv')
 df4 = pd.read_csv('csv')
 
+# sepcify the market for each df - these are examples, will change with new data
 df2['MARKET (agency/dept/client)'] = 'USAF'
 df3['MARKET (agency/dept/client)'] = 'ARMY'
 df4['MARKET (agency/dept/client)'] = 'NAVY'
 
+# split first and last name for all df
 df2[['First Name', 'Last Name']] = df2['Name'].str.split(' ', expand=True, n=1)
 df2.drop('Name', axis=1, inplace=True)
 df2['Last Name'] = df2['Last Name'].str.split(',').str[0]
@@ -24,53 +28,9 @@ df4.drop('Name', axis=1, inplace=True)
 df4['Last Name'] = df4['Last Name'].str.split(',').str[0]
 df4['Last Name'] = df4['Last Name'].str.split(' ').str[0]
 
+# merge into one, drop duplicates, and sort 
 df_new = pd.concat([df, df2, df3, df4]).drop_duplicates(['First Name','Last Name', 'Experience', 'Description/Bio'],keep='last').sort_values('First Name')
 df_new = pd.concat([df, df2, df3, df4]).sort_values('First Name')
-#
-# print(df_new)
-df_new.to_csv('temp_leidos.csv')
 
-
-
-
-
-
-
-
-
-
-
-
-
-# new_df = pd.merge(df, df2, how='inner', on=['First Name', 'Last Name'])
-# # new_df = pd.concat([df,df2], axis=0)
-# new_df.to_csv('NW_trial2.csv')
-# df = pd.read_csv('NW_trial.csv')
-#
-# # df.groupby(['First Name','Last Name'])
-# df.groupby(['First Name', 'Last Name']).agg('sum')
-#
-# df.to_csv('Testing again.csv')
-
-
-
-
-
-
-
-
-
-# df = pd.read_csv('./csv\'s/BAH_PEOPLE_MASTER_NEW.csv')
-# df2 = pd.read_csv('./csv\'s/BAH_NW.csv')
-#
-# df2.columns = ['First Name', 'Last Name', 'Title', 'Location']
-#
-# new_df = pd.merge(df, df2, how='inner', on=['First Name', 'Last Name'])
-# # new_df = pd.concat([df,df2], axis=0)
-# new_df.to_csv('NW_trial2.csv')
-# # df = pd.read_csv('NW_trial.csv')
-# #
-# # # df.groupby(['First Name','Last Name'])
-# # df.groupby(['First Name', 'Last Name']).agg('sum')
-# #
-# # df.to_csv('Testing again.csv')
+# save to temporary csv, later must convert to .xlsx
+df_new.to_csv('temp_master.csv')
